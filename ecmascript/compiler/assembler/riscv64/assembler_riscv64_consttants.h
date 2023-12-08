a50e11ef85b7c8f2660dbc5739fe0af18e3f258f
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2023-12-08 13:58:48 +0800

    rv64-assembler: encoding for r-type inst
    Change-Id: I6d7c8220eaa2fad7c58406c8058975cd5d05d5dc

---
+++ ecmascript/compiler/assembler/riscv64/assembler_riscv64_consttants.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ECMASCRIPT_COMPILER_ASSEMBLER_RISCV64_CONSTANTS_H
+#define ECMASCRIPT_COMPILER_ASSEMBLER_RISCV64_CONSTANTS_H
+namespace panda::ecmascript::riscv64 {
+enum RegisterId : uint8_t {
+    X0, X1, SP, X3, X4, X5, X6, X7,
+    X8, X9, X10, X11, X12, X13, X14, X15,
+    X16, X17, X18, X19, X20, X21, X22, X23,
+    X24, X25, X26, X27, X28, X29, X30, X31,
+    FP = X8,
+    INVALID_REG = 0xFF,
+};
+
+enum RegisterType {
+    W = 0,  /* a word for 32 bits */
+    D = 1,  /* a double-word for 64 bits */
+};
+
+static const int RegDSize = 64;
+static const int RegWSize = 32;
+
+enum AddSubOpFunct {
+    ADD     = 0x00000033,
+    ADDW    = 0x0000003b,
+    SUB     = 0x40000033,
+    SUBW    = 0x4000003b,
+    SLT     = 0x00002033,
+    SLTU    = 0x00003033,
+};
+
+enum ShiftOpFunct {
+    SLL     = 0x00001033,
+    SLLW    = 0x0000103b,
+    SRL     = 0x00005033,
+    SRLW    = 0x0000503b,
+    SRA     = 0x40005033,
+    SRAW    = 0x40005033,
+};
+
+enum BitwiseOpFunct {
+    XOR = 0x00004033,
+    OR  = 0x00006033,
+    AND = 0x00007033,
+};
+};  // namespace panda::ecmascript::riscv64
+#endif
