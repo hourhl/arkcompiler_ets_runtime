2387c3b275fdf7e8d7120e1e59a6419e53cc8ba1
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2023-12-08 20:31:49 +0800

    rv64-assembler-test: add a Test class impl
    Change-Id: I0fd1ec17fb1d0964a5cd2e2c6574f230244c8f6f

---
+++ ecmascript/compiler/assembler/tests/assembler_riscv64_test.cpp
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ecmascript/compiler/assembler/riscv64/assembler_riscv64.h"
+
+#include <ostream>
+#include <sstream>
+
+#include "ecmascript/compiler/assembler/assembler.h"
+#include "ecmascript/ecma_vm.h"
+#include "ecmascript/mem/dyn_chunk.h"
+#include "ecmascript/tests/test_helper.h"
+
+#include "llvm-c/Analysis.h"
+#include "llvm-c/Core.h"
+#include "llvm-c/Disassembler.h"
+#include "llvm-c/ExecutionEngine.h"
+#include "llvm-c/Target.h"
+
+namespace panda::test {
+using namespace panda::ecmascript;
+using namespace panda::ecmascript::riscv64;
+class AssemblerRISCV64Test : public testing::Test {
+public:
+    static void SetUpTestCase()
+    {
+        GTEST_LOG_(INFO) << "SetUpTestCase";
+    }
+
+    static void TearDownTestCase()
+    {
+        GTEST_LOG_(INFO) << "TearDownCase";
+    }
+
+    void SetUp() override
+    {
+        InitializeLLVM(TARGET_RISCV64);
+        TestHelper::CreateEcmaVMWithScope(instance, thread, scope);
+        chunk_ = thread->GetEcmaVM()->GetChunk();
+    }
+
+    void TearDown() override
+    {
+        TestHelper::DestroyEcmaVMWithScope(instance, scope);
+    }
+
+    static const char *SymbolLookupCallback([[maybe_unused]] void *disInfo, [[maybe_unused]] uint64_t referenceValue,
+                                            uint64_t *referenceType, [[maybe_unused]] uint64_t referencePC,
+                                            [[maybe_unused]] const char **referenceName)
+    {
+        *referenceType = LLVMDisassembler_ReferenceType_InOut_None;
+        return nullptr;
+    }
+
+    void InitializeLLVM(std::string triple)
+    {
+        if (triple.compare(TARGET_RISCV64) == 0) {
+            LLVMInitializeRISCVTargetInfo();
+            LLVMInitializeRISCVTargetMC();
+            LLVMInitializeRISCVDisassembler();
+            LLVMInitializeRISCVAsmPrinter();
+            LLVMInitializeRISCVAsmParser();
+            LLVMInitializeRISCVTarget();
+        } else {
+            LOG_ECMA(FATAL) << "this branch is unreachable";
+            UNREACHABLE();
+        }
+    }
+
+    void DisassembleChunk(const char *triple, Assembler *assemlber, std::ostream &os)
+    {
+        LLVMDisasmContextRef dcr = LLVMCreateDisasm(triple, nullptr, 0, nullptr, SymbolLookupCallback);
+        uint8_t *byteSp = assemlber->GetBegin();
+        size_t numBytes = assemlber->GetCurrentPosition();
+        unsigned pc = 0;
+        const char outStringSize = 100;
+        char outString[outStringSize];
+        while (numBytes > 0) {
+            size_t InstSize = LLVMDisasmInstruction(dcr, byteSp, numBytes, pc, outString, outStringSize);
+            if (InstSize == 0) {
+                // 8 : 8 means width of the pc offset and instruction code
+                os << std::setw(8) << std::setfill('0') << std::hex << pc << ":" << std::setw(8)
+                   << *reinterpret_cast<uint32_t *>(byteSp) << "maybe constant" << std::endl;
+                pc += 4; // 4 pc length
+                byteSp += 4; // 4 sp offset
+                numBytes -= 4; // 4 num bytes
+            }
+            // 8 : 8 means width of the pc offset and instruction code
+            os << std::setw(8) << std::setfill('0') << std::hex << pc << ":" << std::setw(8)
+               << *reinterpret_cast<uint32_t *>(byteSp) << " " << outString << std::endl;
+            pc += InstSize;
+            byteSp += InstSize;
+            numBytes -= InstSize;
+        }
+        LLVMDisasmDispose(dcr);
+    }
+    EcmaVM *instance {nullptr};
+    JSThread *thread {nullptr};
+    EcmaHandleScope *scope {nullptr};
+    Chunk *chunk_ {nullptr};
+};
+}  // namespace panda::test
