d9b2e205257e0310dbbaa81aa582647c4977deea
Sicheng Tao <taosicheng2001@gmail.com>
2023-12-08 19:48:21 +0800

    rv64-assembler: add assembler_riscv64.cpp

---
+++ ecmascript/compiler/assembler/riscv64/assembler_riscv64.cpp
@@ -0,0 +1,13 @@
+#include <cstdint>
+#include <map>
+
+#include "ecmascript/compiler/assembler/riscv64/assembler_riscv64.h"
+
+#include "ecmascript/base/bit_helper.h"
+#include "ecmascript/ecma_macros.h"
+
+namespace panda::ecmascript::riscv64 {
+using namespace panda::ecmascript::base;
+
+EMIT_INST
+} // panda::ecmascript::riscv64
