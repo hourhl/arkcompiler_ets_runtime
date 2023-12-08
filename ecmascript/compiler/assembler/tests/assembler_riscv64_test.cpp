1dc60c87a17ba32a63e07fa0ac0159409ef57094
llh730 <2597763106@qq.com>
2023-12-08 22:44:32 +0800

    complete  all test in  32bit R-type
    Change-Id: If1c4a702d3e7f866c43baf33e974401007394c14

--- ecmascript/compiler/assembler/tests/assembler_riscv64_test.cpp
+++ ecmascript/compiler/assembler/tests/assembler_riscv64_test.cpp
@@ -119,37 +119,69 @@ HWTEST_F_L0(AssemblerRiscv64Test, AddSub)
             "00000000:002080b3 \tadd\tra, ra, sp\n"
             "00000004:003100b3 \tadd\tra, sp, gp\n"
             "00000008:41f887b3 \tsub\ta5, a7, t6\n"
-            "0000000c:416409b3 \tsub\ts3, s0, s6\n");
+            "0000000c:416409b3 \tsub\ts3, s0, s6\n" 
+            "00000010:01f8a7b3 \tslt\ta5, a7, t6\n"
+            "00000014:016429b3 \tslt\ts3, s0, s6\n"
+            "00000018:01f8b7b3 \tsltu\ta5, a7, t6\n"
+            "0000001c:016439b3 \tsltu\ts3, s0, s6\n"
+            );
     AssemblerRiscv64 masm(chunk_);
     __ Add(Register(RA), Register(RA), Register(SP));
     __ Add(Register(RA), Register(SP), Register(GP));
     __ Sub(Register(A5), Register(A7), Register(T6));
     __ Sub(Register(S3), Register(FP), Register(S6));
-
+    __ Slt(Register(A5), Register(A7), Register(T6));
+    __ Slt(Register(S3), Register(FP), Register(S6));
+    __ Sltu(Register(A5), Register(A7), Register(T6));
+    __ Sltu(Register(S3), Register(FP), Register(S6));
     std::ostringstream oss;
     DisassembleChunk(TARGET_RISCV64, &masm, oss);
     ASSERT_EQ(oss.str(), expectResult);
 }
 
 
-HWTEST_F_L0(AssemblerRiscv64Test, SllSrl)
+HWTEST_F_L0(AssemblerRiscv64Test, Shift)
 {
     std::string expectResult(
             "00000000:01f897b3 \tsll\ta5, a7, t6\n"
             "00000004:016419b3 \tsll\ts3, s0, s6\n"
             "00000008:01f8d7b3 \tsrl\ta5, a7, t6\n"
-            "0000000c:016459b3 \tsrl\ts3, s0, s6\n");
+            "0000000c:016459b3 \tsrl\ts3, s0, s6\n"
+            "00000010:41f8d7b3 \tsra\ta5, a7, t6\n"
+            "00000014:416459b3 \tsra\ts3, s0, s6\n");
     AssemblerRiscv64 masm(chunk_);
     __ Sll(Register(A5), Register(A7), Register(T6));
     __ Sll(Register(S3), Register(FP), Register(S6));
     __ Srl(Register(A5), Register(A7), Register(T6));
     __ Srl(Register(S3), Register(FP), Register(S6));
+    __ Sra(Register(A5), Register(A7), Register(T6));
+    __ Sra(Register(S3), Register(FP), Register(S6));
     std::ostringstream oss;
     DisassembleChunk(TARGET_RISCV64, &masm, oss);
     ASSERT_EQ(oss.str(), expectResult);
 }
 
 
+HWTEST_F_L0(AssemblerRiscv64Test, BitwiseOp)
+{
+    std::string expectResult(
+            "00000000:01f8c7b3 \txor\ta5, a7, t6\n"
+            "00000004:016449b3 \txor\ts3, s0, s6\n"
+            "00000008:01f8e7b3 \tor\ta5, a7, t6\n"
+            "0000000c:016469b3 \tor\ts3, s0, s6\n"
+            "00000010:01f8f7b3 \tand\ta5, a7, t6\n"
+            "00000014:016479b3 \tand\ts3, s0, s6\n");
+    AssemblerRiscv64 masm(chunk_);
+    __ Xor(Register(A5), Register(A7), Register(T6));
+    __ Xor(Register(S3), Register(FP), Register(S6));
+    __ Or(Register(A5), Register(A7), Register(T6));
+    __ Or(Register(S3), Register(FP), Register(S6));
+    __ And(Register(A5), Register(A7), Register(T6));
+    __ And(Register(S3), Register(FP), Register(S6));
+    std::ostringstream oss;
+    DisassembleChunk(TARGET_RISCV64, &masm, oss);
+    ASSERT_EQ(oss.str(), expectResult);
+}
 
 #undef __
 }  // namespace panda::test
